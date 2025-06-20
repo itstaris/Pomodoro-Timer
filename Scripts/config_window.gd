03f1d3e07dbd6f7f6b13810c825321f9f32ca51b
Tamires <tamires.ei04@gmail.com>
2025-06-20 00:33:05 -0300

    a

--- Scripts/config_window.gd
+++ Scripts/config_window.gd
@@ -1,1 +1,8 @@
 extends Window
+
+func _on_save_button_pressed():
+	Globals.pomodoro = int(work_time_input.text)
+	Globals.short_break = int(short_break_input.text)
+	Globals.long_break = int(long_break_input.text)
+	
+	Globals.save_config()
