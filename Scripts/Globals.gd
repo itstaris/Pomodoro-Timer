b88e38167bad777f60c99796b4b9b4ecad4e8c33
Tamires <tamires.ei04@gmail.com>
2025-06-24 13:23:55 -0300

    .

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var pomodoro = 1
+var pomodoro = 30
 var short_break = 5
 var long_break = 15
 
--- Scripts/config_window.gd
+++ Scripts/config_window.gd
@@ -1,8 +1,21 @@
-extends Window
+extends Panel
 
-func _on_save_button_pressed():
-	Globals.pomodoro = int(work_time_input.text)
-	Globals.short_break = int(short_break_input.text)
-	Globals.long_break = int(long_break_input.text)
+@export var work_spinbox : SpinBox
+@export var short_break_spinbox : SpinBox
+@export var long_break_spinbox : SpinBox
+
+func _ready() -> void:
+	work_spinbox.value = Globals.pomodoro
+	short_break_spinbox.value = Globals.short_break
+	long_break_spinbox.value = Globals.long_break
+
+func _on_save_button_button_up() -> void:
+	Globals.pomodoro = work_spinbox.value
+	Globals.short_break = short_break_spinbox.value
+	Globals.long_break = long_break_spinbox.value
+	
+	print(Globals.pomodoro)
+	print(Globals.short_break)
+	print(Globals.long_break)
 	
 	Globals.save_config()
